f69f3308ff018bb094ec6af5e83f883880839d74
Mikhail Brudkov <mikha484@gmail.com>
2022-05-18 23:26:33 +0300

    modal ready

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
-const discount_button = document.querySelector('.discount-button');
+const modal_overlay = document.querySelector('.modal-overlay');
+const discount_button = document.querySelector('.discount-button__text');
+const modal_close = document.querySelector('.modal-close');
+const discount_close = document.querySelector('.discount-button__close');
+const discount_button_all = document.querySelector('.discount-button');
+
+discount_button.addEventListener('click', function() {
+    modal_overlay.classList.add('open-modal');
+});
+modal_close.addEventListener('click', function() {
+    modal_overlay.classList.remove('open-modal');
+});
+discount_close.addEventListener('click', function() {
+    discount_button_all.classList.add('close-discount');
+});
